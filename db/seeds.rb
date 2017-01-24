e332b91ba85bfca95a57ff27eba99c09ffcc62f2
ruby 13 <ruby.13@metralha.cec.ime.usp.br>
2017-01-24 21:46:47 -0200

    models

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+movies = [
+          {title: "Alladin", release_date: '25-nov-1992'},
+          {title: "When Harry Met Sally", release_date: '25-nov-1989'},
+          {title: "The help", release_date: '10-aug-2001'},
+          {title: "Raiders of the Lost Ark", release_date: '12-jun-1981'}
+         ]
+
+movies.each do |movie|
+  Movie.create!(movie)
+end
